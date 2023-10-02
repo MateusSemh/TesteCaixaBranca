e85cdcd61f9ae8dfa8d37c1bef32d252ecc43283
Mateus Sem H <103202120+MateusSemh@users.noreply.github.com>
2023-10-02 10:45:19 -0300

    Numerar o código

--- Teste.java
+++ Teste.java
@@ -6,30 +6,30 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-	public Connection conectarBD() {
+         1-    	public Connection conectarBD() {
 		Connection conn = null;
-		try {
-			Class.forName("com.mysql.Driver.Manager").newInstance();
-			String url =  "jdbc:mysql://127.0.0.1/test7user=lopes&password=123";
-			conn = DriverManager.getConnection(url);		
-		} catch (Exception e) { }
-		return conn;}
+   2-	     	try {
+		     	Class.forName("com.mysql.Driver.Manager").newInstance();
+		     	String url =  "jdbc:mysql://127.0.0.1/test7user=lopes&password=123";
+		4-	conn = DriverManager.getConnection(url);		
+		} catch (Exception e) { }  -3
+	5-	return conn;}
 	public String nome="";
 	public boolean result = false;
 	public boolean verificarUsuario(String login, String senha) {
 		String sql = "";
 		Connection conn = conectarBD();
 		//INSTRUÇÃO SQL
-		sql += "select nome from usuarios";
+	6-	sql += "select nome from usuarios";
 		sql +="where login= " + "'" + login + "'";
 		sql += " and senha = " + "'" + senha + "';";
 		try {
 			Statement st = conn.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			if(rs.next()) {
-				result = true;
-				nome = rs.getString("nome");}
-		}catch (Exception e) { }
-		return result; }														
+		7-	if(rs.next()) {
+			8-	result = true;
+			10-	nome = rs.getString("nome");}
+		}catch (Exception e) { } -9
+	11-	return result; }														
 	}//fim da class
 
